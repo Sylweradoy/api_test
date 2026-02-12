9e225aab1b65da51a61b8772250b588204dfd662
Sylwek <sylweradoy@wp.pl>
2026-02-12 01:05:28 +0100

    zmiana nr16

--- [...all].js
+++
@@ -1,7 +0,0 @@
-const app = require("./app/app");        // <- Twoje express app (ten plik co pokazałeś)
-const connectDB = require("./app/db/connect");
-
-module.exports = async (req, res) => {
-  await connectDB();
-  return app(req, res);
-};
