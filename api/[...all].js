12ceb7f4959fc66bf9cbeb0d92feb4c54f1191a9
Sylwek <sylweradoy@wp.pl>
2026-02-12 01:16:54 +0100

    zmiana nr 20

--- api/[...all].js
+++ api/[...all].js
@@ -1,5 +1,7 @@
 const app = require("../app/app");
+const connectDB = require("../app/db/connect");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+  await connectDB();
   return app(req, res);
 };
