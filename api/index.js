6af6442f320af3ee2a67110bdc7453f8bc769b9b
Sylwek <sylweradoy@wp.pl>
2026-02-12 19:49:05 +0100

    zmiana nr30

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const app = require("./app/app");
-const connectDB = require("./app/db/connect");
+const app = require("../app/app");
+const connectDB = require("../app/db/connect");
 
 module.exports = async (req, res) => {
   try {
