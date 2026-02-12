0c14733103f862772e34a05d78586ea3610ad0a0
Sylwek <sylweradoy@wp.pl>
2026-02-12 19:46:32 +0100

    zmiana nr20

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const app = require("./app/app");
+const connectDB = require("./app/db/connect");
+
+module.exports = async (req, res) => {
+  try {
+    await connectDB();
+    return app(req, res);
+  } catch (err) {
+    console.error("Vercel handler error:", err);
+    return res.status(500).json({ ok: false, error: "SERVER_ERROR" });
+  }
+};
